979b41fc55284f2283ed07dba9450b76f7e763d0
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2023-03-10 07:25:42 -0700

    initial setup

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,17 +1,39 @@
-// wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
-}
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
-
-// retrieve data from localstorage
+// get data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
+}
+
+// function to take an optional object and a template and insert the objects as HTML into the DOM
+export function renderTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  //if there is a callback...call it and pass data
+  if (callback) {
+    callback(data);
+  }
+}
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+// load header and footer
+export async function loadPartials() {
+  const headTemplate = await loadTemplate("../partials/head.html");
+  const headElement = document.querySelector("head");
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("footer");
+
+  renderTemplate(headTemplate, headElement);
+  renderTemplate(headerTemplate, headerElement);
+  renderTemplate(footerTemplate, footerElement);
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
